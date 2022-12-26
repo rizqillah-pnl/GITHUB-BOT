922b924ab6eb39b3e4b3856646e0a44425ec7874
Rizqillah <rizqillah531@gmail.com>
2022-12-26 14:49:59 +0700

    change code

--- index.js
+++ index.js
@@ -3,27 +3,25 @@ const moment = require('moment');
 const simpleGit = require('simple-git');
 // import {random} from 'random';
 const FILE_PATH = './data.json';
+let i = 1;
 
 const makeCommit = n => {
   if(n==0) return simpleGit().push();
-  const res = [jsonfile.readFile(FILE_PATH)];
 
-  const x = Math.floor(Math.random()*54);
-  const y = Math.floor(Math.random()*12);
+  const x = Math.floor(Math.random()*30);
+  const y = Math.floor(Math.random()*11);
   const DATE = moment().subtract(1, 'y').add(1,'d').add(x, 'w').add(y,'d').format();
 
   const data = {
     date: DATE
   }
-  console.log(DATE);
+  console.log(`${i++} |  ${DATE}`);
 
-  res.push(data);
-
-  jsonfile.writeFile(FILE_PATH, res, ()=>{
+  jsonfile.writeFile(FILE_PATH, data, ()=>{
     simpleGit().add([FILE_PATH]).commit(DATE, {'--date':DATE},makeCommit.bind(this, --n));
   });
 }
 
-makeCommit(2);
+makeCommit(200);
 
 
